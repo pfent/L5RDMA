5841fb8f113c3b4412e0925f71530e4bbd3b6357
Wolf Roediger <roediger@in.tum.de>
2015-10-12 16:44:16 +0200

    introduce RDMA test class

--- main.cpp
+++ main.cpp
@@ -20,55 +20,73 @@
 //---------------------------------------------------------------------------
 #include <iomanip>
 #include <iostream>
+#include <memory>
 //---------------------------------------------------------------------------
 #include "Network.hpp"
 //---------------------------------------------------------------------------
 using namespace std;
 using namespace rdma;
 //---------------------------------------------------------------------------
+int MESSAGE_SIZE = 100;
+//---------------------------------------------------------------------------
+class RDMATest {
+   int nodes;
+   int id;
+   Network network;
+
+public:
+   /// Constructor
+   RDMATest(int nodes, int id) : nodes(nodes), id(id), network(nodes) {
+      vector<Address> addresses(nodes);
+      for (int node = 0; node != nodes; ++node) {
+         if (node == id) {
+            addresses[node] = {network.getLID(), network.getQPN(node)};
+         } else {
+            cout << "[" << node << "] " << network.getLID() << " " << network.getQPN(node) << endl;
+            cin >> addresses[node].lid;
+            cin >> addresses[node].qpn;
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+         }
+      }
+      network.connect(addresses);
+   }
+
+   void testSendReceive() {
+      // receive message
+      char *receive = new char[MESSAGE_SIZE];
+      MemoryRegion receiveMR(receive, MESSAGE_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::LocalWrite);
+      network.postRecv(receiveMR, 0);
+      int ignore;
+      cout << "[ENTER MESSAGE]" << endl;
+      string message;
+      getline(cin, message);
+
+      // send message
+      char *send = new char[MESSAGE_SIZE];
+      snprintf(send, MESSAGE_SIZE, "%s", message.c_str());
+      MemoryRegion sendMR(send, MESSAGE_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::LocalWrite | MemoryRegion::Permission::RemoteWrite);
+      network.postSend((id+1)%nodes, sendMR, true, 0);
+      network.waitForCompletionSend();
+      network.waitForCompletionReceive();
+      cout << "message: " << receive << endl;
+
+      delete[] receive;
+      delete[] send;
+   }
+
+   void testAtomics() {
+   
+   }
+};
+//---------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
    if (argc != 3) {
       cerr << "Usage: nodes id" << endl;
       exit(EXIT_FAILURE);
    }
-
-   int MESSAGE_SIZE = 100;
-   int NODES = atoi(argv[1]);
-   int ID = atoi(argv[2]);
-
-   // connect network
-   vector<Address> addresses(NODES);
-   Network network(NODES);
-   for (int node = 0; node != NODES; ++node) {
-      if (node == ID) {
-         addresses[node] = {network.getLID(), network.getQPN(node)};
-      } else {
-         cout << "[" << node << "] " << network.getLID() << " " << network.getQPN(node) << endl;
-         cin >> addresses[node].lid;
-         cin >> addresses[node].qpn;
-         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      }
-   }
-   network.connect(addresses);
-
-   // receive message
-   char *receive = new char[MESSAGE_SIZE];
-   MemoryRegion receiveMR(receive, MESSAGE_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::LocalWrite);
-   network.postRecv(receiveMR, 0);
-   int ignore;
-   cout << "[ENTER MESSAGE]" << endl;
-   string message;
-   getline(cin, message);
-
-   // send message
-   char *send = new char[MESSAGE_SIZE];
-   snprintf(send, MESSAGE_SIZE, "%s", message.c_str());
-   MemoryRegion sendMR(send, MESSAGE_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::LocalWrite | MemoryRegion::Permission::RemoteWrite);
-   network.postSend((ID+1)%NODES, sendMR, true, 0);
-   network.waitForCompletionSend();
-   network.waitForCompletionReceive();
-   cout << "message: " << receive << endl;
-
-   delete[] receive;
-   delete[] send;
+   int nodes = atoi(argv[1]);
+   int id = atoi(argv[2]);
+   
+   RDMATest test(nodes, id);
+   test.testAtomics();
 }
