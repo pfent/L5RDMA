50e5cdfb841decfa484ae516d4b5023ddb8bf3b9
pfent <philipp@fent.de>
2016-12-06 20:31:39 +0100

    exchange qpn (2)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <algorithm>
-#include <chrono>
 #include <rdma_tests/rdma/CompletionQueuePair.hpp>
 #include <rdma_tests/rdma/QueuePair.hpp>
+#include <rdma_tests/rdma/MemoryRegion.hpp>
+#include <rdma_tests/rdma/WorkRequest.hpp>
+#include <infiniband/verbs.h>
 #include "rdma/Network.hpp"
 
 using namespace std;
@@ -45,11 +46,10 @@ void tcp_bind(int sock, sockaddr_in &addr) {
     }
 }
 
-struct RDMA_Info {
-    uint32_t QPN; // TODO qpn probably should be separate, because we can't setup a RMR without a network
-    uint32_t rmrKey;
-    uintptr_t rmrAddress;
-};
+void exchangeQPNAndConnect(int sock, Network &network, QueuePair &queuePair);
+
+void receiveAndSetupRmr(int sock, RemoteMemoryRegion &remoteMemoryRegion);
+void sendRmrInfo(int sock, MemoryRegion &sharedMemoryRegion);
 
 int tcp_accept(int sock, sockaddr_in &inAddr) {
     socklen_t inAddrLen = sizeof inAddr;
@@ -66,11 +66,11 @@ int main(int argc, char **argv) {
         return -1;
     }
     const auto isClient = argv[1][0] == 'c';
-    const auto port = atoi(argv[2]);
+    const auto port = ::atoi(argv[2]);
 
     auto sock = tcp_socket();
 
-    static const size_t BUFFER_SIZE = 64;
+    const size_t BUFFER_SIZE = 64;
     uint8_t buffer[BUFFER_SIZE];
     const char DATA[] = "123456789012345678901234567890123456789012345678901234567890123";
     static_assert(BUFFER_SIZE == sizeof(DATA), "DATA needs the right size ");
@@ -80,54 +80,89 @@ int main(int argc, char **argv) {
     CompletionQueuePair completionQueue(network);
     QueuePair queuePair(network, completionQueue);
 
-    const uint32_t  qpn = queuePair.getQPN();
     if (isClient) {
         sockaddr_in addr;
         addr.sin_family = AF_INET;
         addr.sin_port = htons(port);
         inet_pton(AF_INET, argv[3], &addr.sin_addr);
 
-        // TODO:
-        // Recieve the qpn
-        // Setup the Network
-        // Setup shared memory region
-        // Exchange RMR keys && addresses
-        // Setup the Remote memory region
-
         tcp_connect(sock, addr);
-        uint32_t qPNbuffer = htonl(qpn);
-        tcp_write(sock, &qPNbuffer, sizeof(qPNbuffer)); // Send own qpn to server
-        tcp_read(sock, &qPNbuffer, sizeof(qPNbuffer)); // receive qpn
+        exchangeQPNAndConnect(sock, network, queuePair);
+
+        copy(begin(DATA), end(DATA), begin(buffer)); // Send DATA
+        MemoryRegion sharedMR(buffer, BUFFER_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::All);
+        RemoteMemoryRegion remoteMemoryRegion;
+        receiveAndSetupRmr(sock, remoteMemoryRegion);
+
+        WriteWorkRequest workRequest;
+        workRequest.setLocalAddress(sharedMR);
+        workRequest.setRemoteAddress(remoteMemoryRegion);
+        workRequest.setCompletion(true);
+
+        queuePair.postWorkRequest(workRequest);
+        auto a = completionQueue.waitForCompletion();
+        cout << "Completed: " << a.first << " " << a.second << endl;
     } else {
         sockaddr_in addr;
         addr.sin_family = AF_INET;
         addr.sin_port = htons(port);
         addr.sin_addr.s_addr = INADDR_ANY;
 
-        // TODO:
-        // Recieve the qpn
-        // Setup the Network
-        // Setup shared memory region
-        // Exchange RMR keys && addresses
-        // Setup the Remote memory region
-
         tcp_bind(sock, addr);
         listen(sock, SOMAXCONN);
         sockaddr_in inAddr;
         for (;;) {
             auto acced = tcp_accept(sock, inAddr);
-            tcp_read(acced, buffer, BUFFER_SIZE);
-            cout << "received: '";
-            for (const auto c : buffer) {
-                cout << c;
-            }
-            cout << "'" << endl;
-            tcp_write(acced, buffer, BUFFER_SIZE);
+            exchangeQPNAndConnect(acced, network, queuePair);
+
+            MemoryRegion sharedMR(buffer, BUFFER_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::All);
+            sendRmrInfo(acced, sharedMR);
+
+            // TODO: maintain a bytes to read / bytes to write count
+            int qwe;
+            cout << "waiting for data ..." << endl;
+            cin >> qwe;
+
+            cout << buffer;
+
             close(acced);
         }
     }
 
     close(sock);
     return 0;
+}
+
+struct RmrInfo {
+    uint32_t key;
+    uintptr_t address;
+    static_assert(sizeof(uintptr_t) == sizeof(uint64_t), "Only 64bit platforms supported");
+};
+
+void receiveAndSetupRmr(int sock, RemoteMemoryRegion &remoteMemoryRegion) {
+    RmrInfo rmrInfo;
+    tcp_read(sock, &rmrInfo, sizeof(rmrInfo));
+    rmrInfo.key = ntohl(rmrInfo.key);
+    rmrInfo.address = be64toh(rmrInfo.address);
+    remoteMemoryRegion.key = rmrInfo.key;
+    remoteMemoryRegion.address = rmrInfo.address;
+}
+
+void sendRmrInfo(int sock, MemoryRegion &sharedMemoryRegion) {
+    RmrInfo rmrInfo;
+    rmrInfo.key = htonl(sharedMemoryRegion.key->rkey);
+    rmrInfo.address = htobe64((uint64_t) sharedMemoryRegion.address);
+    tcp_write(sock, &rmrInfo, sizeof(rmrInfo));
+}
+
+void exchangeQPNAndConnect(int sock, Network &network, QueuePair &queuePair) {
+    uint32_t qpn = queuePair.getQPN();
+    uint32_t qPNbuffer = htonl(qpn);
+    tcp_write(sock, &qPNbuffer, sizeof(qPNbuffer)); // Send own qpn to server
+    tcp_read(sock, &qPNbuffer, sizeof(qPNbuffer)); // receive qpn
+    qpn = ntohl(qPNbuffer);
+    const Address address{network.getLID(), qpn};
+    queuePair.connect(address);
+    cout << "connected to qpn " << qpn << endl;
 }
 
