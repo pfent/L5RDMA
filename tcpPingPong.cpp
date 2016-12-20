44ff7115fd387b6bbaeed17b4e88feac0ca2b34b
pfent <philipp@fent.de>
2016-12-20 12:17:37 +0100

    tcpPingPong

--- tcpPingPong.cpp
+++ tcpPingPong.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <algorithm>
@@ -24,8 +23,11 @@ int main(int argc, char **argv) {
         throw runtime_error{"Could not open socket"};
     }
 
+    static const size_t MESSAGES = 1024;
     static const size_t BUFFER_SIZE = 64;
     uint8_t buffer[BUFFER_SIZE];
+    uint8_t DATA[] = "123456789012345678901234567890123456789012345678901234567890123";
+    static_assert(BUFFER_SIZE == sizeof(buffer), "DATA needs the right size ");
 
     if (isClient) {
         sockaddr_in addr;
@@ -36,26 +38,26 @@ int main(int argc, char **argv) {
         if (connect(sock, (sockaddr *) &addr, sizeof addr) < 0) {
             throw runtime_error{"error connect'ing"};
         }
-        unsigned char a = ':'; // ':' + 64 is 'z'
-        for (auto &c : buffer) {
-            c = a++;
-        }
         const auto start = chrono::steady_clock::now();
-        if (write(sock, buffer, BUFFER_SIZE) < 0) {
-            throw runtime_error{"error write'ing"};
-        }
-        fill(begin(buffer), end(buffer), 0);
-        if (read(sock, buffer, BUFFER_SIZE) < 0) {
-            throw runtime_error{"error read'ing"};
-        }
-        a = ':';
-        for (const auto c : buffer) {
-            if (c != a++) {
-                throw runtime_error{"expected ':~z', received " + string(begin(buffer), end(buffer))};
+        for (size_t i = 0; i < MESSAGES; ++i) {
+            if (write(sock, buffer, BUFFER_SIZE) < 0) {
+                throw runtime_error{"error write'ing"};
+            }
+            fill(begin(buffer), end(buffer), 0);
+            if (read(sock, buffer, BUFFER_SIZE) < 0) {
+                throw runtime_error{"error read'ing"};
+            }
+            for (size_t j = 0; j < BUFFER_SIZE; ++j) {
+                if (buffer[j] != DATA[j]) {
+                    throw runtime_error{"expected '1~9', received " + string(begin(buffer), end(buffer))};
+                }
             }
         }
         const auto end = chrono::steady_clock::now();
-        cout << "RTT: " << chrono::duration<double, milli>(end - start).count() << "ms" << endl;
+        const auto msTaken = chrono::duration<double, milli>(end - start).count();
+        const auto sTaken = msTaken / 1000;
+        cout << MESSAGES << " messages exchanged in " << msTaken << "ms" << endl;
+        cout << MESSAGES / sTaken << " msg/s";
     } else {
         sockaddr_in addr;
         addr.sin_family = AF_INET;
@@ -72,16 +74,13 @@ int main(int argc, char **argv) {
             if (acced < 0) {
                 throw runtime_error{"error accept'ing"};
             }
-            if (read(acced, buffer, BUFFER_SIZE) < 0) {
-                throw runtime_error{"error read'ing"};
-            }
-            cout << "received: '";
-            for (const auto c : buffer) {
-                cout << c;
-            }
-            cout << "'" << endl;
-            if (write(acced, buffer, BUFFER_SIZE) < 0) {
-                throw runtime_error{"error write'ing"};
+            for (size_t i = 0; i < MESSAGES; ++i) {
+                if (read(acced, buffer, BUFFER_SIZE) < 0) {
+                    throw runtime_error{"error read'ing"};
+                }
+                if (write(acced, buffer, BUFFER_SIZE) < 0) {
+                    throw runtime_error{"error write'ing"};
+                }
             }
             close(acced);
         }
