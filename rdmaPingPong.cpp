fa0187df1281923a8af70b13ee521ef6fc8e80ae
pfent <philipp@fent.de>
2017-01-06 14:01:43 +0100

    move the send / receive functionalty to a RDMAMessageBuffer

--- rdmaPingPong.cpp
+++ rdmaPingPong.cpp
@@ -2,6 +2,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 #include <rdma_tests/rdma/CompletionQueuePair.hpp>
 #include <rdma_tests/rdma/QueuePair.hpp>
 #include <rdma_tests/rdma/MemoryRegion.hpp>
@@ -19,6 +20,69 @@ void receiveAndSetupRmr(int sock, RemoteMemoryRegion &buffer);
 
 void sendRmrInfo(int sock, MemoryRegion &buffer);
 
+struct RDMANetworking {
+    Network network;
+    CompletionQueuePair completionQueue;
+    QueuePair queuePair;
+
+    RDMANetworking(int sock) :
+            completionQueue(network),
+            queuePair(network, completionQueue) {
+        exchangeQPNAndConnect(sock, network, queuePair);
+    }
+};
+
+template<size_t BUFFER_SIZE>
+class RDMAMessageBuffer {
+    static const size_t validity = 0xDEADDEADBEEFBEEF;
+public:
+    void send(uint8_t *data, size_t length) {
+        const size_t sizeToWrite = sizeof(length) + length + sizeof(validity);
+        if (sizeToWrite > size) throw std::runtime_error{"data > buffersize!"};
+        // TODO: current position and wraparound
+        auto *begin = sendBuffer;
+        memcpy(begin, &length, sizeof(length));
+        memcpy(begin + sizeof(length), data, length);
+        memcpy(begin + sizeof(length) + length, &validity, sizeof(validity));
+
+        WriteWorkRequestBuilder(localSend, remoteReceive, true)
+                .send(net.queuePair);
+        net.completionQueue.pollSendCompletionQueue(IBV_WC_RDMA_WRITE); // This probably leaves one completion in the CQ
+    }
+
+    vector<uint8_t> receive() {
+        volatile uint8_t *begin = receiveBuffer; // TODO: current position and wraparound
+        volatile auto *receiveSize = (volatile size_t *) begin;
+        while (*receiveSize == 0);
+        auto messageSize = *receiveSize;
+        // TODO: check if size is in bounds or do wraparound
+        volatile auto *receiveValidity = (volatile decltype(validity) *) (begin + sizeof(size_t) + messageSize);
+        while (*receiveValidity == 0);
+        if (*receiveValidity != validity) throw std::runtime_error{"unexpected validity " + *receiveValidity};
+        auto result = vector<uint8_t>(begin + sizeof(size_t), begin + sizeof(size_t) + messageSize);
+        fill(begin, begin + sizeof(messageSize) + messageSize + sizeof(validity), 0);
+        return move(result);
+    }
+
+    RDMAMessageBuffer(int sock) :
+            net(sock),
+            localSend(sendBuffer, size, net.network.getProtectionDomain(), MemoryRegion::Permission::All),
+            localReceive((void *) receiveBuffer, size, net.network.getProtectionDomain(),
+                         MemoryRegion::Permission::All) {
+        sendRmrInfo(sock, localReceive);
+        receiveAndSetupRmr(sock, remoteReceive);
+    }
+
+private:
+    static const size_t size = BUFFER_SIZE;
+    RDMANetworking net;
+    volatile uint8_t receiveBuffer[size]{0};
+    uint8_t sendBuffer[size];
+    MemoryRegion localSend;
+    MemoryRegion localReceive;
+    RemoteMemoryRegion remoteReceive;
+};
+
 int main(int argc, char **argv) {
     if (argc < 3 || (argv[1][0] == 'c' && argc < 4)) {
         cout << "Usage: " << argv[0] << " <client / server> <Port> [IP (if client)]" << endl;
@@ -32,11 +96,6 @@ int main(int argc, char **argv) {
     static const size_t MESSAGES = 1024 * 128;
     static const size_t BUFFER_SIZE = 64;
 
-    // RDMA networking. The queues are needed on both sides
-    Network network;
-    CompletionQueuePair completionQueue(network);
-    QueuePair queuePair(network, completionQueue);
-
     if (isClient) {
         sockaddr_in addr;
         addr.sin_family = AF_INET;
@@ -44,35 +103,27 @@ int main(int argc, char **argv) {
         inet_pton(AF_INET, argv[3], &addr.sin_addr);
 
         tcp_connect(sock, addr);
-        exchangeQPNAndConnect(sock, network, queuePair);
 
-        volatile uint8_t receiveBuffer[BUFFER_SIZE]{0};
-        uint8_t sendBuffer[] = "123456789012345678901234567890123456789012345678901234567890123";
-        static_assert(BUFFER_SIZE == sizeof(sendBuffer), "sendBuffer needs the right size ");
-        MemoryRegion localSend(sendBuffer, BUFFER_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::All);
-        MemoryRegion localReceive((void *) receiveBuffer, BUFFER_SIZE, network.getProtectionDomain(),
-                                  MemoryRegion::Permission::All);
-        RemoteMemoryRegion remoteReceive;
-        sendRmrInfo(sock, localReceive);
-        receiveAndSetupRmr(sock, remoteReceive);
+        auto sendData = array<uint8_t, 64>{"123456789012345678901234567890123456789012345678901234567890123"};
+        RDMAMessageBuffer<128> rdma(sock);
 
         const auto start = chrono::steady_clock::now();
         for (size_t i = 0; i < MESSAGES; ++i) {
-            WriteWorkRequestBuilder(localSend, remoteReceive, true)
-                    .send(queuePair);
-            completionQueue.pollSendCompletionQueue(IBV_WC_RDMA_WRITE);
-            while (receiveBuffer[BUFFER_SIZE - 2] == 0); // sync with response
+            rdma.send(sendData.data(), sendData.size());
+            auto answer = rdma.receive();
+            if (answer.size() != sendData.size()) {
+                throw runtime_error{"answer has wrong size!"};
+            }
             for (size_t j = 0; j < BUFFER_SIZE; ++j) {
-                if (receiveBuffer[j] != sendBuffer[j]) {
-                    throw runtime_error{"expected '1~9', received " + string(begin(receiveBuffer), end(receiveBuffer))};
+                if (answer[j] != sendData[j]) {
+                    throw runtime_error{"expected '1~9', received " + string(answer.begin(), answer.end())};
                 }
             }
-            fill(begin(receiveBuffer), end(receiveBuffer), 0);
         }
         const auto end = chrono::steady_clock::now();
         const auto msTaken = chrono::duration<double, milli>(end - start).count();
         const auto sTaken = msTaken / 1000;
-        cout << MESSAGES << " " << sizeof(sendBuffer) << "B messages exchanged in " << msTaken << "ms" << endl;
+        cout << MESSAGES << " " << sendData.size() << "B messages exchanged in " << msTaken << "ms" << endl;
         cout << MESSAGES / sTaken << " msg/s" << endl;
     } else {
         sockaddr_in addr;
@@ -85,24 +136,12 @@ int main(int argc, char **argv) {
         sockaddr_in inAddr;
 
         auto acced = tcp_accept(sock, inAddr);
-        exchangeQPNAndConnect(acced, network, queuePair);
 
-        volatile uint8_t receiveBuffer[BUFFER_SIZE]{0};
-        uint8_t sendBuffer[BUFFER_SIZE]{0};
-        MemoryRegion localReceive((void *) receiveBuffer, BUFFER_SIZE, network.getProtectionDomain(),
-                                  MemoryRegion::Permission::All);
-        MemoryRegion localSend(sendBuffer, BUFFER_SIZE, network.getProtectionDomain(), MemoryRegion::Permission::All);
-        RemoteMemoryRegion remoteReceive;
-        sendRmrInfo(acced, localReceive);
-        receiveAndSetupRmr(acced, remoteReceive);
+        RDMAMessageBuffer<128> rdma(acced);
 
         for (size_t i = 0; i < MESSAGES; ++i) {
-            while (receiveBuffer[BUFFER_SIZE - 2] == 0);
-            copy(begin(receiveBuffer), end(receiveBuffer), begin(sendBuffer));
-            fill(begin(receiveBuffer), end(receiveBuffer), 0);
-            WriteWorkRequestBuilder(localSend, remoteReceive, true)
-                    .send(queuePair);
-            completionQueue.pollSendCompletionQueue(IBV_WC_RDMA_WRITE);
+            auto ping = rdma.receive();
+            rdma.send(ping.data(), ping.size());
         }
 
         close(acced);
