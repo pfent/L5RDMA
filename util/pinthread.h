3f2da03ef979a3687735d850df40245c16ea792a
pfent <philipp@fent.de>
2018-01-14 22:54:18 +0100

    properly do pinThread

--- util/pinthread.h
+++ util/pinthread.h
@@ -4,18 +4,18 @@
 #include <pthread.h>
 #include <stdexcept>
 
-void pinThread(int cpu) {
-    auto thread = pthread_self();
+void pinThread(size_t cpu) {
+    const auto thread = pthread_self();
     cpu_set_t cpuset{};
     CPU_ZERO(&cpuset);
     CPU_SET(cpu, &cpuset);
-    if (pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset) != 0) {
-        perror("pthread_setaffinity_np");
-        throw std::runtime_error("pthread_setaffinity_np failed");
+    auto res = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+    if (res != 0) {
+        throw std::runtime_error("pthread_setaffinity_np failed with "s + strerror(res));
     }
-    if (pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset) != 0) {
-        perror("pthread_getaffinity_np");
-        throw std::runtime_error("pthread_getaffinity_np failed");
+    res = pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+    if (res != 0) {
+        throw std::runtime_error("pthread_getaffinity_np failed with "s + strerror(res));
     }
 }
 
