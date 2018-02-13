0a6bfcc0e0cd084a919b6eb0f14a3c1ba26d5753
pfent <philipp@fent.de>
2018-02-13 18:43:21 +0100

    proper formatting

--- util/tcpWrapper.cpp
+++ util/tcpWrapper.cpp
@@ -10,36 +10,36 @@ using namespace std::string_literals;
 int tcp_socket() {
     auto sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        throw std::runtime_error{"Could not open socket"s + strerror(errno)};
+        throw std::runtime_error{"Could not open socket: "s + strerror(errno)};
     }
     const int enable = 1;
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
-        throw std::runtime_error{"Could not set SO_REUSEADDR"s + strerror(errno)};
+        throw std::runtime_error{"Could not set SO_REUSEADDR: "s + strerror(errno)};
     }
     return sock;
 }
 
 void tcp_connect(int sock, const sockaddr_in &addr) {
     if (connect(sock, reinterpret_cast<const sockaddr *>(&addr), sizeof addr) < 0) {
-        throw std::runtime_error{"error connect'ing"s + strerror(errno)};
+        throw std::runtime_error{"error connect'ing: "s + strerror(errno)};
     }
 }
 
 void tcp_write(int sock, const void *buffer, std::size_t size) {
     if (send(sock, buffer, size, 0) < 0) {
-        throw std::runtime_error{"error write'ing"s + strerror(errno)};
+        throw std::runtime_error{"error write'ing: "s + strerror(errno)};
     }
 }
 
 void tcp_read(int sock, void *buffer, std::size_t size) {
     if (recv(sock, buffer, size, 0) < 0) {
-        throw std::runtime_error{"error read'ing"s + strerror(errno)};
+        throw std::runtime_error{"error read'ing: "s + strerror(errno)};
     }
 }
 
 void tcp_bind(int sock, const sockaddr_in &addr) {
     if (bind(sock, reinterpret_cast<const sockaddr *>(&addr), sizeof addr) < 0) {
-        throw std::runtime_error{"error bind'ing"s + strerror(errno)};
+        throw std::runtime_error{"error bind'ing: "s + strerror(errno)};
     }
 }
 
@@ -47,7 +47,7 @@ int tcp_accept(int sock, sockaddr_in &inAddr) {
     socklen_t inAddrLen = sizeof inAddr;
     auto acced = accept(sock, reinterpret_cast<sockaddr *>(&inAddr), &inAddrLen);
     if (acced < 0) {
-        throw std::runtime_error{"error accept'ing"s + strerror(errno)};
+        throw std::runtime_error{"error accept'ing: "s + strerror(errno)};
     }
     return acced;
 }
@@ -60,12 +60,12 @@ void tcp_setBlocking(int sock) {
 
 void tcp_close(int sock) {
     if (close(sock) < 0) {
-        throw std::runtime_error{"error close'ing"s + strerror(errno)};
+        throw std::runtime_error{"error close'ing: "s + strerror(errno)};
     }
 }
 
 void tcp_listen(int sock) {
     if (listen(sock, SOMAXCONN) < 0) {
-        throw std::runtime_error{"error listen'ing"s + strerror(errno)};
+        throw std::runtime_error{"error listen'ing: "s + strerror(errno)};
     }
 }
