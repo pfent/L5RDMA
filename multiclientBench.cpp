3323e7ff8deaa76a209806f0de5f4e4358507507
pfent <philipp@fent.de>
2018-04-01 16:13:30 +0200

    add multiclientBench

---
+++ multiclientBench.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <thread>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <transports/MulticlientTransport.h>
+#include "libibverbscpp/libibverbscpp.h"
+#include "rdma/Network.hpp"
+#include "rdma/QueuePair.hpp"
+#include "util/tcpWrapper.h"
+#include "util/bench.h"
+#include "rdma/RcQueuePair.h"
+#include "rdma/UcQueuePair.h"
+#include "rdma/UdQueuePair.h"
+
+using namespace std;
+
+constexpr uint16_t port = 1234;
+const char *ip = "127.0.0.1";
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <client / server> <(optional) 127.0.0.1>" << endl;
+        return -1;
+    }
+    const auto isClient = argv[1][0] == 'c';
+    if (argc > 2) {
+        ip = argv[2];
+    }
+
+    if (isClient) {
+        auto client = MultiClientTransportClient();
+        client.connect(string(ip) + to_string(port));
+
+        const char *testdata = "asdfghjkl";
+        std::vector<char> buf(10);
+
+        // TODO: repeat
+        client.send(reinterpret_cast<const uint8_t *>(testdata), 10);
+        client.receive(buf.data(), 10);
+
+        for (size_t i = 0; i < 10; ++i) {
+            if (testdata[i] != buf[i]) throw runtime_error("NEQ");
+        }
+    } else {
+        auto server = MulticlientTransportServer(to_string(port));
+        server.accept();
+
+        std::vector<uint8_t> buf(10);
+        // TODO: repeat
+        auto client = server.receive(buf.data(), 10);
+        server.send(client, buf.data(), 10);
+    }
+}
