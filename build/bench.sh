23ed01584803e5ddf3a6317b3372aa9cae4be6b0
pfent <philipp@fent.de>
2018-04-04 13:36:09 +0200

    do proper benchmarking

--- build/bench.sh
+++ build/bench.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/env sh
-./bench server > /dev/null &
-sleep 1
-./bench client | column -s, -t
+#!/usr/bin/env bash
+set -e
+if [[ $# -lt 1 ]]; then
+    echo "Please specify client / server, optionally IP"
+    exit
+fi
+
+BENCHMARKS=$(ls | grep "Bench$")
+NODE=1
+for test in $BENCHMARKS; do
+    echo numactl --membind="$NODE" --cpunodebind="$NODE" ./"$test" "$1" "$2" \| tee "$test"_"$1".csv \| column -s, -t
+    numactl --membind="$NODE" --cpunodebind="$NODE" ./"$test" "$1" "$2" | tee "$test"_"$1".csv | column -s, -t
+done
