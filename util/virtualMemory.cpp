50b36fed5839bfa25887a6d2c0ddbee7a96d265a
Philipp Fent <fent@in.tum.de>
2018-12-12 16:05:19 +0100

    fix shm leaking

--- util/virtualMemory.cpp
+++ util/virtualMemory.cpp
@@ -31,6 +31,11 @@ WraparoundBuffer mmapRingBuffer(int fd, size_t size, bool init) {
     auto ptr = reinterpret_cast<uint8_t *>(
             mmap(nullptr, size * 2, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)
     );
+    const auto deleter = [size](void *p) {
+        // unmap the whole continuous memory mapping
+        munmap(p, size * 2);
+    };
+
     // map the shared memory to the first half
     if (mmap(ptr, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, fd, 0) != ptr) {
         throw std::runtime_error{std::string("mmaping the first wraparound failed ") + strerror(errno)};
@@ -39,14 +44,12 @@ WraparoundBuffer mmapRingBuffer(int fd, size_t size, bool init) {
     if (mmap(&ptr[size], size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, fd, 0) != &ptr[size]) {
         throw std::runtime_error{std::string("mmaping the second wraparound failed ") +  + strerror(errno)};
     }
+    // because of the overlap, we need no deleter and unmapping for those mappings
 
     if (init) {
         memset(ptr, 0, size);
     }
 
-    const auto deleter = [size](void *p) {
-        munmap(p, size);
-    };
     return WraparoundBuffer(fd, {ptr, deleter});
 }
 } // namespace util
